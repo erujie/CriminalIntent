9ef28fee074d01d626dd9657f943d9ea1f625fd1
erujie <erjaydosdos@gmail.com>
2026-03-17 22:08:58 +0800

    chapter 11 + challenges complete

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -106,7 +106,7 @@ public class CrimeListFragment extends Fragment {
         @Override
         public void onClick(View view) {
             mLastUpdatedPosition = getBindingAdapterPosition();
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
     }
---
+++ app/src/main/java/com/example/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,108 @@
+package com.example.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity  extends AppCompatActivity {
+    private static final String EXTRA_CRIME_ID =
+            "com.example.criminalintent.crime_id";
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+    private Button mJumpToFirstButton;
+    private Button mJumpToLastButton;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId) {
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent()
+                .getSerializableExtra(EXTRA_CRIME_ID);
+
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+
+        mJumpToFirstButton = (Button) findViewById(R.id.jump_to_first_button);
+        mJumpToLastButton = (Button) findViewById(R.id.jump_to_last_button);
+        mJumpToFirstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+        mJumpToLastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size() - 1);
+            }
+        });
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Not needed
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                updateButtons();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Not needed
+            }
+        });
+
+        for (int i = 0; i < mCrimes.size(); i++) {
+            if (mCrimes.get(i).getId().equals(crimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+
+        updateButtons();
+
+    }
+
+    private void updateButtons() {
+        int currentItem = mViewPager.getCurrentItem();
+
+        mJumpToFirstButton.setEnabled(currentItem != 0);
+        mJumpToLastButton.setEnabled(currentItem != mCrimes.size() - 1);
+    }
+}
